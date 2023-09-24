0500b238a17649ac88f7207096cc90d5a67a04b8
samuel <sammykuria300@gmail.com>
2023-09-24 13:57:03 +0300

    Quick commit

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+
+    bestCustomer = "maybe bob"
+    return bestCustomer
+}
+
+const leastFavouriteCustomer = 'James'
+
+function changeLeastFavoriteCustomer(newCustomer) {
+
+     leastFavouriteCustomer = newCustomer
+     return leastFavouriteCustomer
+
+}
+changeLeastFavoriteCustomer(Alex)
